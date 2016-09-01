4f5d5cb960272c696d8572c8ace9bdfd63283d94
r3dskhie <wilf.pineda@yahoo.com>
2016-09-01 11:55:28 +0800

    Update KantoTrees.lua

--- Kanto/KantoTrees.lua
+++ Kanto/KantoTrees.lua
@@ -22,47 +22,54 @@
 	  		return talkToNpcOnCell(, )	--Tree 4
 	  	elseif isNpcOnCell(, ) then
 	  		return talkToNpcOnCell(, )	--Tree 5
-	  	if isNpcOnCell() then
-  			return talkToNpcOnCell(, )	--Tree 1
+	  	elseif isNpcOnCell() then
+  			return talkToNpcOnCell(, )	--Tree 6
   		elseif isNpcOnCell(, ) then
-			return talkToNpcOnCell(, )	--Tree 2
+			return talkToNpcOnCell(, )	--Tree 7
 		elseif isNpcOnCell(, ) then
-			return talkToNpcOnCell(, )	--Tree 3
+			return talkToNpcOnCell(, )	--Tree 8
   		elseif isNpcOnCell(, ) then
-	  		return talkToNpcOnCell(, )	--Tree 4
+	  		return talkToNpcOnCell(, )	--Tree 9
 	  	elseif isNpcOnCell(, ) then
-	  		return talkToNpcOnCell(, )	--Tree 5
-	  	if isNpcOnCell() then
-  			return talkToNpcOnCell(, )	--Tree 1
+	  		return talkToNpcOnCell(, )	--Tree 10
+	  	elseif isNpcOnCell() then
+  			return talkToNpcOnCell(, )	--Tree 11
   		elseif isNpcOnCell(, ) then
-			return talkToNpcOnCell(, )	--Tree 2
+			return talkToNpcOnCell(, )	--Tree 12
 		elseif isNpcOnCell(, ) then
-			return talkToNpcOnCell(, )	--Tree 3
+			return talkToNpcOnCell(, )	--Tree 13
   		elseif isNpcOnCell(, ) then
-	  		return talkToNpcOnCell(, )	--Tree 4
+	  		return talkToNpcOnCell(, )	--Tree 14
 	  	elseif isNpcOnCell(, ) then
-	  		return talkToNpcOnCell(, )	--Tree 5
-	  	if isNpcOnCell() then
-  			return talkToNpcOnCell(, )	--Tree 1
+	  		return talkToNpcOnCell(, )	--Tree 15
+	  	elseif isNpcOnCell() then
+  			return talkToNpcOnCell(, )	--Tree 16
   		elseif isNpcOnCell(, ) then
-			return talkToNpcOnCell(, )	--Tree 2
+			return talkToNpcOnCell(, )	--Tree 17
 		elseif isNpcOnCell(, ) then
-			return talkToNpcOnCell(, )	--Tree 3
+			return talkToNpcOnCell(, )	--Tree 18
   		elseif isNpcOnCell(, ) then
-	  		return talkToNpcOnCell(, )	--Tree 4
+	  		return talkToNpcOnCell(, )	--Tree 19
 	  	elseif isNpcOnCell(, ) then
-	  		return talkToNpcOnCell(, )	--Tree 5
+	  		return talkToNpcOnCell(, )	--Tree 20
 	  	if isNpcOnCell() then
-  			return talkToNpcOnCell(, )	--Tree 1
+  			return talkToNpcOnCell(, )	--Tree 21
   		elseif isNpcOnCell(, ) then
-			return talkToNpcOnCell(, )	--Tree 2
+			return talkToNpcOnCell(, )	--Tree 22
 		elseif isNpcOnCell(, ) then
-			return talkToNpcOnCell(, )	--Tree 3
-  		elseif isNpcOnCell(, ) then
-	  		return talkToNpcOnCell(, )	--Tree 4
-	  	elseif isNpcOnCell(, ) then
-	  		return talkToNpcOnCell(, )	--Tree 5
+			return talkToNpcOnCell(, )	--Tree 23
+  		end
  	elseif getMapName() == "Viridian City" then
+ 		if isNpcOnCell() then
+  			return talkToNpcOnCell(, )	--Tree 1
+  		elseif isNpcOnCell(, ) then
+			return talkToNpcOnCell(, )	--Tree 2
+		elseif isNpcOnCell(, ) then
+			return talkToNpcOnCell(, )	--Tree 3
+  		elseif isNpcOnCell(, ) then
+	  		return talkToNpcOnCell(, )	--Tree 4
+	  	elseif isNpcOnCell(, ) then
+	  		return talkToNpcOnCell(, )	--Tree 5
 	elseif getMapName() == "Pewter City" then
 	elseif getMapName() == "Lavender Town" then
 	elseif getMapName() == "Cerulean City" then
