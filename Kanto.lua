470eb812e326110374878700a6aa3853b0902281
r3dskhie <wilf.pineda@yahoo.com>
2016-09-01 09:58:37 +0800

    Update Kanto.lua

--- Kanto.lua
+++ Kanto.lua
@@ -1,2 +1,35 @@
 
-
+function onPathAction()
+  
+  if getMapName() == "Pallet Town" then
+    if isNpcOnCell(x,y) then
+      return talkToNpcOnCell(x,y)
+    elseif isNpcOnCell(x,y) then
+      return talkToNpcOnCell(x,y)
+    elseif isNpcOnCell(x,y) then
+      return talkToNpcOnCell(x,y)
+    elseif isNpcOnCell(x,y) then
+      return talkToNpcOnCell(x,y)
+    elseif isNpcOnCell(x,y) then
+      return talkToNpcOnCell(x,y)
+    end
+  elseif getMapName() == "Route 1" then
+    if isNpcOnCell(x,y) then
+      return talkToNpcOnCell(x,y)
+    elseif isNpcOnCell(x,y) then
+      return talkToNpcOnCell(x,y)
+    elseif isNpcOnCell(x,y) then
+      return talkToNpcOnCell(x,y)
+    elseif isNpcOnCell(x,y) then
+      return talkToNpcOnCell(x,y)
+    elseif isNpcOnCell(x,y) then
+      return talkToNpcOnCell(x,y)
+    elseif isNpcOnCell(x,y) then
+      return talkToNpcOnCell(x,y)
+    elseif isNpcOnCell(x,y) then
+      return talkToNpcOnCell(x,y)
+    elseif isNpcOnCell(x,y) then
+      return talkToNpcOnCell(x,y)
+    end
+  
+  
