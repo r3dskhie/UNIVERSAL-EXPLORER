31719f29ae4614e5c25e604714e3e3b6d7e06683
r3dskhie <wilf.pineda@yahoo.com>
2016-09-01 12:08:10 +0800

    Update Headbutt.lua

--- Headbutt.lua
+++ Headbutt.lua
@@ -1,22 +1,37 @@
 
 dofile "Kanto"
-dofile "Johto"
-
-local trees = Kanto
-
-function Kantrees()
-for _,trees in pairs(trees) do
-  if trees == isNpcOnCell() then
-    	talkToNpcOnCell(trees)
-    break
-  end
-end
-end
 
 funtion onPathAction()
+
   if isPokemonUsable(1) then
-    if getMapName() == "Cerulean City" then
-      return Kantrees() or moveToMap("Route 4")
+    
+    if getMapName() == "Pallet Town" then
+      moveToMap("Route 1")
+    elseif getMapName() == "Route 1" then
+      moveToMap("Viridian City")
+    elseif getMapName() == "Viridian City" then
+      moveToMap("Route 1")
+    elseif getMapName() == "Pallet Town" then
+      moveToMap("Route 1")
+    elseif getMapName() == "Pallet Town" then
+      moveToMap("Route 1")
+    elseif getMapName() == "Pallet Town" then
+      moveToMap("Route 1")
+    elseif getMapName() == "Pallet Town" then
+      moveToMap("Route 1")
+    elseif getMapName() == "Pallet Town" then
+      moveToMap("Route 1")
+    elseif getMapName() == "Pallet Town" then
+      moveToMap("Route 1")
+    elseif getMapName() == "Pallet Town" then
+      moveToMap("Route 1")
+    elseif getMapName() == "Pallet Town" then
+      moveToMap("Route 1")
+    elseif getMapName() == "Pallet Town" then
+      moveToMap("Route 1")
+    elseif getMapName() == "Pallet Town" then
+      moveToMap("Route 1")
     end
+    
   end
 end
