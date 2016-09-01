b73247e5f9b72bdb17e52df32d3d1ce62b7e3cab
r3dskhie <wilf.pineda@yahoo.com>
2016-09-01 10:22:47 +0800

    Update KantoTrees.lua

--- KantoTrees.lua
+++ KantoTrees.lua
@@ -1,5 +1,19 @@
 Kanto = {}
 Kanto = ["Pallet Town"] = {(x,y), (x,y), (x,y), (x,y), (x,y)}
-Kanto = ["Pallet Town"] = {(x,y), (x,y), (x,y), (x,y), (x,y)}
-Kanto = ["Pallet Town"] = {(x,y), (x,y), (x,y), (x,y), (x,y)}
-Kanto = ["Pallet Town"] = {(x,y), (x,y), (x,y), (x,y), (x,y)}
+Kanto = ["Viridian Forest"] = {(x,y), (x,y), (x,y), (x,y), (x,y), (x,y), (x,y), (x,y), (x,y), (x,y), (x,y), (x,y), (x,y), (x,y), (x,y), (x,y), (x,y), (x,y), (x,y), (x,y), (x,y), (x,y), (x,y), (x,y), (x,y)(x,y), (x,y), (x,y)}
+Kanto = ["Viridian City"] = {(x,y), (x,y), (x,y), (x,y), (x,y), (x,y), (x,y), (x,y)}
+Kanto = ["Pewter City"] = {(x,y), (x,y), (x,y), (x,y), (x,y), (x,y), (x,y), (x,y), (x,y), (x,y), (x,y), (x,y), (x,y)}
+Kanto = ["Cerulean City"] = {(x,y), (x,y), (x,y), (x,y), (x,y), (x,y), (x,y)}
+Kanto = ["Lavender Town"] = {(x,y), (x,y), (x,y)}
+Kanto = ["Route 1"] = {(x,y), (x,y), (x,y), (x,y), (x,y)}
+Kanto = ["Route 2"] = {(x,y), (x,y), (x,y), (x,y), (x,y)}
+Kanto = ["Route 3"] = {(x,y), (x,y), (x,y), (x,y), (x,y)}
+Kanto = ["Route 4"] = {(x,y), (x,y), (x,y), (x,y), (x,y)}
+Kanto = ["Route 7"] = {(x,y), (x,y), (x,y), (x,y), (x,y)}
+Kanto = ["Route 8"] = {(x,y), (x,y), (x,y), (x,y), (x,y)}
+Kanto = ["Route 11"] = {(x,y), (x,y), (x,y), (x,y), (x,y)}
+Kanto = ["Route 13"] = {(x,y), (x,y), (x,y), (x,y), (x,y)}
+Kanto = ["Route 14"] = {(x,y), (x,y), (x,y), (x,y), (x,y)}
+Kanto = ["Route 15"] = {(x,y), (x,y), (x,y), (x,y), (x,y)}
+Kanto = ["Route 22"] = {(x,y), (x,y), (x,y), (x,y), (x,y)}
+
