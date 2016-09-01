860cd745db9b8c21391028f78a8430c561e1a4c0
r3dskhie <wilf.pineda@yahoo.com>
2016-09-01 13:22:04 +0800

    Update KantoTrees.lua

--- Kanto/KantoTrees.lua
+++ Kanto/KantoTrees.lua
@@ -1,17 +1,32 @@
 	
 	if getMapName() == "Pallet Town" then
-  		if isNpcOnCell() then
+  		if isNpcOnCell(28, 22) then
+			pushDialogAnswer(5)
+			log("---Headbutting first tree---")
   			return talkToNpcOnCell(28, 22)	--Tree 1
-  		elseif isNpcOnCell(, ) then
-			return talkToNpcOnCell(27, 17)	--Tree 2
-		elseif isNpcOnCell(, ) then
+  		elseif isNpcOnCell(27, 16) then
+			pushDialogAnswer(5)
+			log("----Headbutting second tree---")
+			return talkToNpcOnCell(27, 16)	--Tree 2
+		elseif isNpcOnCell(20, 7) then
+			pushDialogAnswer(5)
+			log("----Headbutting third tree---")
 			return talkToNpcOnCell(20, 7)	--Tree 3
-  		elseif isNpcOnCell(, ) then
-	  		return talkToNpcOnCell(8, 5)	--Tree 4
-	  	elseif isNpcOnCell(, ) then
-	  		return talkToNpcOnCell(4, 7)	--Tree 5
-	  	elseif isNpcOnCell(, ) then
-	  		return talkToNpcOnCell(3, 17)	--Tree 6
+		elseif isNpcOnCell(8, 5) then
+			pushDialogAnswer(5)
+			log("----Headbutting fourth tree---")
+			return talkToNpcOnCell(8, 5)	--Tree 4
+		elseif isNpcOnCell(4, 7) then
+			pushDialogAnswer(5)
+			log("----Headbutting fifth tree---")
+			return talkToNpcOnCell(4, 7)	--Tree 5
+	  	elseif isNpcOnCell(13, 17) then
+			pushDialogAnswer(5)
+			log("----Headbutting sixth tree---")
+			return talkToNpcOnCell(13, 17)	--Tree 6
+		else
+			log("---"..getMapName().." Cleared... Moving to next Map---")
+			Destination()
   		end
 	elseif getMapName() == "Viridian Forest" then
 		if isNpcOnCell() then
