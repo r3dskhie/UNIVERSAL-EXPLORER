ad16f55e017005857cbb55eae5248f89d31fafe2
r3dskhie <wilf.pineda@yahoo.com>
2016-09-01 10:09:19 +0800

    Update Kanto.lua

--- Kanto.lua
+++ Kanto.lua
@@ -1,8 +1,74 @@
 
-function onPathAction()
+function Headbutt()
   
   if getMapName() == "Pallet Town" then
     if isNpcOnCell(x,y) then
+      return talkToNpcOnCell(x,y)
+    elseif isNpcOnCell(x,y) then
+      return talkToNpcOnCell(x,y)
+    elseif isNpcOnCell(x,y) then
+      return talkToNpcOnCell(x,y)
+    elseif isNpcOnCell(x,y) then
+      return talkToNpcOnCell(x,y)
+    elseif isNpcOnCell(x,y) then
+      return talkToNpcOnCell(x,y)
+    end
+  elseif getMapName() == "Viridian City" then
+    if isNpcOnCell(x,y) then
+      return talkToNpcOnCell(x,y)
+    elseif isNpcOnCell(x,y) then
+      return talkToNpcOnCell(x,y)
+    elseif isNpcOnCell(x,y) then
+      return talkToNpcOnCell(x,y)
+    elseif isNpcOnCell(x,y) then
+      return talkToNpcOnCell(x,y)
+    elseif isNpcOnCell(x,y) then
+      return talkToNpcOnCell(x,y)
+    elseif isNpcOnCell(x,y) then
+      return talkToNpcOnCell(x,y)
+    elseif isNpcOnCell(x,y) then
+      return talkToNpcOnCell(x,y)
+    elseif isNpcOnCell(x,y) then
+      return talkToNpcOnCell(x,y)
+    end
+  elseif getMapName() == "Viridian Forest" then
+    if isNpcOnCell(x,y) then
+      return talkToNpcOnCell(x,y)
+    elseif isNpcOnCell(x,y) then
+      return talkToNpcOnCell(x,y)
+    elseif isNpcOnCell(x,y) then
+      return talkToNpcOnCell(x,y)
+    elseif isNpcOnCell(x,y) then
+      return talkToNpcOnCell(x,y)
+    elseif isNpcOnCell(x,y) then
+      return talkToNpcOnCell(x,y)
+    elseif isNpcOnCell(x,y) then
+      return talkToNpcOnCell(x,y)
+    elseif isNpcOnCell(x,y) then
+      return talkToNpcOnCell(x,y)
+    elseif isNpcOnCell(x,y) then
+      return talkToNpcOnCell(x,y)
+    if isNpcOnCell(x,y) then
+      return talkToNpcOnCell(x,y)
+    elseif isNpcOnCell(x,y) then
+      return talkToNpcOnCell(x,y)
+    elseif isNpcOnCell(x,y) then
+      return talkToNpcOnCell(x,y)
+    elseif isNpcOnCell(x,y) then
+      return talkToNpcOnCell(x,y)
+    elseif isNpcOnCell(x,y) then
+      return talkToNpcOnCell(x,y)
+    elseif isNpcOnCell(x,y) then
+      return talkToNpcOnCell(x,y)
+    elseif isNpcOnCell(x,y) then
+      return talkToNpcOnCell(x,y)
+    elseif isNpcOnCell(x,y) then
+      return talkToNpcOnCell(x,y)
+    elseif isNpcOnCell(x,y) then
+      return talkToNpcOnCell(x,y)
+    elseif isNpcOnCell(x,y) then
+      return talkToNpcOnCell(x,y)
+    elseif isNpcOnCell(x,y) then
       return talkToNpcOnCell(x,y)
     elseif isNpcOnCell(x,y) then
       return talkToNpcOnCell(x,y)
