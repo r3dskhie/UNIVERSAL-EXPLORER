4f1d12076d156de3544e16f724da64ef53d04cd6
r3dskhie <wilf.pineda@yahoo.com>
2016-09-01 10:37:13 +0800

    Update JohtoTrees.lua

--- Johto/JohtoTrees.lua
+++ Johto/JohtoTrees.lua
@@ -1,1 +1,23 @@
+Johto = {}
+
+Johto = ["Cherrygrove City"] = {(x,y), (x,y), (x,y), (x,y), (x,y)}
+Johto = ["Ilex Forest"] = {(x,y), (x,y), (x,y), (x,y), (x,y), (x,y), (x,y), (x,y), (x,y), (x,y), (x,y), (x,y), (x,y), (x,y), (x,y), (x,y), (x,y), (x,y), (x,y), (x,y), (x,y), (x,y), (x,y), (x,y), (x,y)(x,y), (x,y), (x,y)}
+Johto = ["Violet City"] = {(x,y), (x,y), (x,y), (x,y), (x,y), (x,y), (x,y), (x,y)}
+Johto = ["Azalea Town"] = {(x,y), (x,y), (x,y), (x,y), (x,y), (x,y), (x,y), (x,y), (x,y), (x,y), (x,y), (x,y), (x,y)}
+Johto = ["Route 26"] = {(x,y), (x,y), (x,y), (x,y), (x,y), (x,y), (x,y), (x,y)}
+Johto = ["Route 27"] = {(x,y), (x,y), (x,y)}
+Johto = ["Route 29"] = {(x,y), (x,y), (x,y), (x,y), (x,y)}
+Johto = ["Route 30"] = {(x,y), (x,y), (x,y), (x,y), (x,y)}
+Johto = ["Route 31"] = {(x,y), (x,y), (x,y), (x,y), (x,y), (x,y), (x,y), (x,y), (x,y), (x,y), (x,y), (x,y)}
+Johto = ["Route 32"] = {(x,y), (x,y), (x,y), (x,y), (x,y)}
+Johto = ["Route 33"] = {(x,y), (x,y), (x,y), (x,y), (x,y)}
+Johto = ["Route 34"] = {(x,y), (x,y), (x,y), (x,y), (x,y), (x,y), (x,y), (x,y), (x,y), (x,y), (x,y), (x,y), (x,y), (x,y), (x,y)}
+Johto = ["Route 35"] = {(x,y), (x,y), (x,y), (x,y), (x,y)}
+Johto = ["Route 36"] = {(x,y), (x,y), (x,y), (x,y), (x,y)}
+Johto = ["Route 38"] = {(x,y), (x,y), (x,y), (x,y), (x,y)}
+Johto = ["Route 42"] = {(x,y), (x,y), (x,y), (x,y), (x,y)}
+Johto = ["Route 44"] = {(x,y), (x,y), (x,y), (x,y), (x,y)}
+Johto = ["Route 46"] = {(x,y), (x,y), (x,y), (x,y), (x,y)}
+
+return Johto
 
