6c766ef01e4c179df4bbf157337553470cd186ab
r3dskhie <wilf.pineda@yahoo.com>
2016-09-04 13:41:28 +0800

    Update Hooooo.lua

--- Bugs/Hooooo.lua
+++ Bugs/Hooooo.lua
@@ -1,1 +1,20 @@
 
+function onPathAction()
+  if isPokemonUsable(1)
+    if getMapName() == "Lilycove City" then
+        moveToMap("Route 121")
+    elseif getMapName() == "Route 121" then
+        moveToMap("Route 120")
+    elseif getMapName() == "120" then
+        moveToMap("Fortree City")
+    elseif getMapName() == "Fortree City" then
+        moveToMap("Route 119A")
+    elseif getMapName() == "Route 119A" then
+        talkToNpcOnCell(16,68)
+    elseif getMapName() == "Natural Site" then
+        moveToCell(6,7)
+    elseif getMapName() == "Route 119A" then
+        moveToMap("Route 119B")
+    elseif getMapName() == "Route 119B" then
+        moveToMap("Route 118")
+  end
