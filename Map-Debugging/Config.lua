27e1edef307552866eca8af814020dfc075640cc
r3dskhie <wilf.pineda@yahoo.com>
2016-09-05 19:27:54 +0800

    Update Config.lua

--- Map-Debugging/Config.lua
+++ Map-Debugging/Config.lua
@@ -1,1 +1,58 @@
+				-- Copyright © 2016 redskhie 
+				-- Official github link:
+				-- Official PROShine thread: https://proshine-bot.com/thread-1246.html
+				-- Contact me at discord nickname: Redd
 
+
+
+
+        backupCount = 0		--Do not touch this if you're not in Hoenn Region
+        			--Always Remember Back Up Count when Doing Excavation Sites
+        			--Enter your Back Up Count if you are DISCONNECTED in the middle of the Excavation Sites
+        			
+        			
+	mount = "Bicycle"	--Choose what kind of Mount you want to use, I suggest you to use any kind of mount for faster routine
+	
+	
+	
+digger = 4		--Set the index number of Pokemon with digs
+butter = 5		--Set the index number of Pokemon with headbutt
+
+
+	MtMoon = false		--Set this to false if the dig spots are on cooldown to not encounter an error on MT. MOON
+
+
+
+	--CATCHING CONFIGURATION
+	
+		--PLEASE SET TO normal catch if you're not using Synchronize pokemon
+		
+catchUncaught = true		--set to true if you want to catch uncaught pokes else false
+catchShiny = true		--set to true if you want to catch encountered shiny's else false
+
+		--Add names of the pokemon you want to catch if not on this list
+		--REMEMBER that pokemon names are sensitive
+catchList = {"Starly", "Fletchling", "Chimchar", "Scyther", "Murkrow", "Staravia", "Turtwig", "Venipede", "Fletchinder", "Rufflet", "Golett", "Gligar", "Noibat", "Aron", "Beldum", "Mawile", "Absol", "Pawniard", "Litleo", "Vanillite", "Skorupi", "Trapinch", "Ferroseed", "Sawk", "Throh", "Timburr"}
+
+		--PLEASE SET TO normal catch if you're not using Synchronize pokemon
+		
+normalCatchOn =true        --Directly throw pokeballs on hunted pokemons
+fswipeOn = false       --Switch to pokemon with false swipe, use false swipe until the hunted pokes hp is reach to 1 then throw pokeballs
+sleepOn = false            --Switch to pokemon with sleep move, use sleep move until the hunted pokes status is SLEEPING then throw pokeballs
+advanceCatchOn = false        --Use fswipeOn and sleepOn to catch hunted pokes
+
+falseswiper = 2            --Index of pokemon with false swipe
+fswipeHP = 10            --Returns the health percent of the false swiper, then heal to PC
+weakmove = "False Swipe"    --False Swipe move
+
+sleeper = 6            --Index of pokemon with sleep move
+sleeperHP = 10            --Returns the health percent of the false swiper, then heal to PC
+sleepmove = "Spore"        --Set/Change the name of the sleep move
+
+
+
+
+			-- Copyright © 2016 redskhie 
+				-- Official github link:
+				-- Official PROShine thread: https://proshine-bot.com/thread-1246.html
+				-- Contact me at discord nickname: Redd
